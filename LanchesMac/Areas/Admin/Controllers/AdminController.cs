bfd1cef0bb3816588514ea6b54bdf6bc239d7da1
Matheus F. Laurindo <matheuslaurindo2017@gmail.com>
2023-02-10 13:45:55 -0300

    Implementação das roles para admin e member e autorização de acesso para area administrativa

---
+++ LanchesMac/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.IsInRole("Admin"))
+                return View();
+
+            return RedirectToAction("AccessDenied", "Admin");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
